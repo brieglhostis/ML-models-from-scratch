10ea93d5cc7da26b523025cc861de222b45c2b34
brieglhostis <brieg.lhostis1@gmail.com>
2025-06-01 18:04:12 +0200

    Update scaler with comments and safety parameter

--- scalers.py
+++ scalers.py
@@ -3,17 +3,25 @@ import numpy as np
 
 
 class NormalScaler:
+    """
+    Normal scaler class to transform X into:
+    X_scaled = (X - X_mean) / (X_var + epsilon)**0.5
+    """
     
-    def __init__(self):
+    def __init__(self, axis=0, epsilon=1e-6):
+        self.axis = axis
+        self.epsilon = epsilon
         self.mean = None
+        self.var = None
         self.std = None
         
     def fit(self, x):
-        self.mean = x.mean(0)
-        self.std = x.std(0)
+        self.mean = np.mean(x, axis=self.axis, keepdims=True)
+        self.var = np.var(x, axis=self.axis, keepdims=True)
+        self.std = np.sqrt(self.var + self.epsilon)
         
     def transform(self, x):
-        return (x-self.mean) / self.std
+        return (x - self.mean) / self.std
     
     def fit_transform(self, x):
         self.fit(x)
