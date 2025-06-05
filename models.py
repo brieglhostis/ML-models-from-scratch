7957a644c4507a4286e191e4c2cc2d69539a7db9
brieglhostis <brieg.lhostis1@gmail.com>
2025-06-05 21:24:59 +0200

    Add Adam to RNN

--- models.py
+++ models.py
@@ -69,7 +69,7 @@ class LinearRegression:
 
 class RegressionNeuralNetwork:
     """
-    Neural Network model:
+    Regression Neural Network model:
     Y = activation_f(X W + b)
     Generalised to N layers
     """
@@ -147,7 +147,7 @@ class RegressionNeuralNetwork:
             activation_input = layer.forward(layer_input, inference=inference) 
             # Activation forward pass
             batch_normalization_input = activation.forward(activation_input) 
-             # Batch norm forward pass
+            # Batch norm forward pass
             layer_input = batch_normalization.forward(batch_normalization_input, inference=inference) if batch_normalization is not None else batch_normalization_input
             if not inference:
                 layer_inputs += [activation_input, batch_normalization_input, layer_input]
@@ -163,7 +163,7 @@ class RegressionNeuralNetwork:
         Arguments:
          - X (np.ndarray)              - input training features (NxF)
          - Y (np.ndarray)              - target training actuals (NxD)
-         - X_val (np.ndarray)          - input validation actuals (N'xD)
+         - X_val (np.ndarray)          - input validation actuals (N'xF)
          - Y_val (np.ndarray)          - target validation actuals (N'xD)
          - learning_rate (float)       - learning rate in gradient descent
          - epochs (int)                - number of optimization steps
@@ -199,8 +199,6 @@ class RegressionNeuralNetwork:
             Y_batches = [Y[i*batch_size:min(X.shape[0],(i+1)*batch_size)] for i in range(X.shape[0] // batch_size)]
             
         self.history = []
-        past_layer_gradient_norms = [{} for layer in self.layers]
-        past_batch_normalization_gradient_norms = [{} for layer in self.layers]
         for e in range(epochs):
             # Fitting
             for X_batch, Y_batch in zip(X_batches, Y_batches):
@@ -257,54 +255,99 @@ class RegressionNeuralNetwork:
 
 
 class RegressionRecurrentNeuralNetwork:
+    """
+    Recurrent regression Neural Network model:
+    Y = activation_f(X W + b)
+    Generalised to N layers and with X and Y being sequential arrays
+    """
     
     LAYER_TYPES = ['Dense', 'Recurrent', 'LSTM', 'BidirectionalRecurrent', 'BidirectionalLSTM']
-    GRADIENT_DESCENT_METHODS = ['Regular', 'RMSProp', 'AdaGrad']
     
     def __init__(
-        self, F, D, layer_sizes=[10], add_bias=[True, True], layer_types=['Recurrent', 'Dense'], activations=[ReLuActivation, LinearActivation]
+        self, F, D, layer_sizes=[10], layer_types=['Recurrent', 'Dense'], add_bias=[True, True], activations=[ReLuActivation, LinearActivation]
         , l2=0.0, dropout_p=0.0, batch_normalization=False, batch_normalization_alpha=0.0):
+        """
+        Initialize the neural network object
+        Arguments:
+         - F (int)                                     - input dimension size
+         - D (int)                                     - output dimension size
+         - layer_sizes (list[int])                     - hidden layers dimension sizes
+         - layer_types (list[str])                     - hidden layers types among 'Dense', 'Recurrent', 'LSTM', 'BidirectionalRecurrent', or 'BidirectionalLSTM'
+         - add_bias (Any[bool, list[bool]])            - set to True to add bias in all layers, or use list to specify layer by layer
+         - activations (list[Activation])              - list of layer activation functions
+         - l2 (float)                                  - L2 regularization parameter
+         - dropout (Any[float, list[float]])           - dropout rate in all layers, or use list to specify layer by layer
+         - batch_normalization (Any[bool, list[bool]]) - set to True to add batch normalization to all layers except the output layer, or use list to specify layer by layer
+         - batch_normalization_alpha (float)           - batch normalization moving average exponential decay parameter
+        """
         
         assert all([t in self.LAYER_TYPES for t in layer_types])
         
         self.F = F
         self.D = D
-        
         self.l2 = l2
-        self.dropout_p = dropout_p if isinstance(dropout_p, list) else [dropout_p] * (len(layer_sizes)+1)
-        self.batch_normalization = batch_normalization if isinstance(batch_normalization, list) else [batch_normalization] * len(layer_sizes)
         
+        # Initialize layers
         self.layer_sizes = layer_sizes
+        self.n_layers = len(self.layer_sizes)+1
+        add_bias = add_bias if isinstance(add_bias, list) else [add_bias] * self.n_layers
+        dropout_p = dropout_p if isinstance(dropout_p, list) else [dropout_p] * self.n_layers
+        batch_normalization = batch_normalization if isinstance(batch_normalization, list) else [batch_normalization] * (self.n_layers-1)
+        assert len(add_bias) == self.n_layers
+        assert len(dropout_p) == self.n_layers
+        assert len(batch_normalization) == self.n_layers-1
         self.layers = [
             RecurrentLayer(f, d, b, dropout_p=p) if t == 'Recurrent' else 
             LSTMLayer(f, d, b, dropout_p=p) if t == 'LSTM' else 
             BidirectionalRecurrentLayer(f, d, b, dropout_p=p) if t == 'BidirectionalRecurrent' else 
             BidirectionalLSTMLayer(f, d, b, dropout_p=p) if t == 'BidirectionalLSTM' else 
             DenseLayer(f, d, b, dropout_p=p)
-            for f, d, b, t, p in zip([F]+self.layer_sizes, self.layer_sizes+[D], add_bias, layer_types, [0.0]+self.dropout_p)
+            for f, d, t, b, p in zip([F]+self.layer_sizes, self.layer_sizes+[D], layer_types, add_bias, dropout_p)
         ]
         self.activations = [
             a() for a in activations
         ]
         self.batch_normalization_layers = [
             BatchNormalization(alpha=batch_normalization_alpha) if b else None
-            for b in self.batch_normalization
+            for b in batch_normalization
         ] + [None]
+        
         self.history = []
         
     def loss(self, X, Y):
+        """
+        Compute the prediction MSE
+        Arguments:
+         - X (np.ndarray) - input features (NxTxF)
+         - Y (np.ndarray) - target actuals (NxTxD)
+        """
         Y_pred = self.predict(X)
         return mse(Y, Y_pred, axis=(0,1))
     
     def predict(self, X):
+        """
+        Compute predictions for a set of input samples 
+        Arguments:
+         - X (np.ndarray) - input features (NxTxF)
+         - Y (np.ndarray) - target actuals (NxTxD)
+        """
         return self.forward(X, inference=True)[-1]
     
     def forward(self, X, inference=True):
+        """
+        Forward pass all layers 
+        Arguments:
+         - X (np.ndarray)   - input features (NxTxF)
+         - inference (bool) - set to True to avoid returning intermediate layer results
+        """
         layer_input = X.copy()
         layer_inputs = [] if inference else [layer_input]
         for layer, activation, batch_normalization in zip(self.layers, self.activations, self.batch_normalization_layers):
+            # Layer forward pass
             activation_input = layer.forward(layer_input, inference=inference)
+            # Activation forward pass
             batch_normalization_input = activation.forward(activation_input)
+            # Batch norm forward pass
             layer_input = batch_normalization.forward(batch_normalization_input, inference=inference) if batch_normalization is not None else batch_normalization_input
             if not inference:
                 layer_inputs += [activation_input, batch_normalization_input, layer_input]
@@ -312,9 +355,40 @@ class RegressionRecurrentNeuralNetwork:
             layer_inputs.append(layer_input)
         return layer_inputs
     
-    def fit(self, X, Y, X_val=None, Y_val=None, lr=0.1, epochs=100, batch_size=100, gradient_descent_method="AdaGrad", gradient_decay=0.8, verbose=True):
+    def fit(
+        self, X, Y, X_val=None, Y_val=None, learning_rate=0.1, epochs=100, batch_size=100
+        , optimizer='Adam', gradient_decay=0.9, gradient_norm_decay=0.999, verbose=True):
+        """
+        Fit neural network using gradient descent to minimize loss
+        Arguments:
+         - X (np.ndarray)              - input training features (NxTxF)
+         - Y (np.ndarray)              - target training actuals (NxTxD)
+         - X_val (np.ndarray)          - input validation actuals (N'xTxF)
+         - Y_val (np.ndarray)          - target validation actuals (N'xTxD)
+         - learning_rate (float)       - learning rate in gradient descent
+         - epochs (int)                - number of optimization steps
+         - batch_size (int)            - size of batches in batch gradient descent
+         - optimizer (str)             - name of the optimizer to use among 'Regular', 'AdaGrad', 'RMSProp', or 'Adam'
+         - gradient_decay (float)      - exponential gradient decay parameter for RMSProp and Adam
+         - gradient_norm_decay (float) - exponential gradient norm decay parameter for Adam
+         - verbose (float)             - set to True to print intermediary evaluation metrics
+        """
         
-        assert gradient_descent_method in self.GRADIENT_DESCENT_METHODS
+        # Optimizers initialization
+        if optimizer == 'Regular':
+            optimizer = GradientDescentOptimizer()
+            batch_norm_optimizer = GradientDescentOptimizer()
+        elif optimizer == 'AdaGrad':
+            optimizer = AdaGradOptimizer()
+            batch_norm_optimizer = AdaGradOptimizer()
+        elif optimizer == 'RMSProp':
+            optimizer = RMSPropOptimizer(gradient_decay=gradient_decay)
+            batch_norm_optimizer = RMSPropOptimizer(gradient_decay=gradient_decay)
+        elif optimizer == 'Adam':
+            optimizer = AdamOptimizer(gradient_decay=gradient_decay, gradient_norm_decay=gradient_norm_decay)
+            batch_norm_optimizer = AdamOptimizer(gradient_decay=gradient_decay, gradient_norm_decay=gradient_norm_decay)
+        else:
+            raise ValueError(f"Optimizer must be one of 'Regular', 'AdaGrad', 'RMSProp', or 'Adam'")
         
         # Batch creation
         if batch_size >= X.shape[0]:
@@ -325,8 +399,6 @@ class RegressionRecurrentNeuralNetwork:
             Y_batches = [Y[i*batch_size:min(X.shape[0],(i+1)*batch_size)] for i in range(X.shape[0] // batch_size)]
         
         self.history = []
-        past_layer_gradient_norms = [{} for layer in self.layers]
-        past_batch_normalization_gradient_norms = [{} for layer in self.layers]
         for e in range(epochs):
             # Fitting
             for X_batch, Y_batch in zip(X_batches, Y_batches):
@@ -350,41 +422,17 @@ class RegressionRecurrentNeuralNetwork:
                     layer_gradient, layer_error = layer.backward(layer_input, layer_error, l2=self.l2)
                     layer_gradients = [layer_gradient] + layer_gradients
                     batch_normalization_gradients = [batch_normalization_gradient] + batch_normalization_gradients
-
+                
                 # Layer updates
-                if gradient_descent_method == 'AdaGrad':
-                    past_layer_gradient_norms = [
-                        {k: (pgn[k] if k in pgn else 0.0)+np.square(g[k]) for k in g} 
-                        for pgn, g in zip(past_layer_gradient_norms, layer_gradients)]
-                elif gradient_descent_method == 'RMSProp':
-                    past_layer_gradient_norms = [
-                        {k: gradient_decay*(pgn[k] if k in pgn else 0.0)+(1-gradient_decay)*np.square(g[k]) for k in g} 
-                        for pgn, g in zip(past_layer_gradient_norms, layer_gradients)]
-                for layer, layer_gradient, past_layer_gradient_norm in zip(self.layers, layer_gradients, past_layer_gradient_norms):
-                    if gradient_descent_method in ('AdaGrad', 'RMSProp'):
-                        kwargs = {k: lr * layer_gradient[k] / np.sqrt(past_layer_gradient_norm[k]+1e-6) for k in layer_gradient}
-                        layer.update(**kwargs)
-                    else:
-                        kwargs = {k: lr * layer_gradient[k] for k in layer_gradient}
-                        layer.update(**kwargs)
-
+                layer_gradients = optimizer.update({'gradients': layer_gradients}, learning_rate=learning_rate)
+                for layer, layer_gradient in zip(self.layers, layer_gradients['gradients']):
+                    layer.update(**layer_gradient)
+                
                 # Batch normalization updates
-                if gradient_descent_method == 'AdaGrad':
-                    past_batch_normalization_gradient_norms = [
-                        {k: (pgn[k] if k in pgn else 0.0)+np.square(g[k]) for k in g} 
-                        for pgn, g in zip(past_batch_normalization_gradient_norms, batch_normalization_gradients)]
-                elif gradient_descent_method == 'RMSProp':
-                    past_batch_normalization_gradient_norms = [
-                        {k: gradient_decay*(pgn[k] if k in pgn else 0.0)+(1-gradient_decay)*np.square(g[k]) for k in g} 
-                        for pgn, g in zip(past_batch_normalization_gradient_norms, batch_normalization_gradients)]
-                for layer, layer_gradient, past_layer_gradient_norm in zip(self.batch_normalization_layers, batch_normalization_gradients, past_batch_normalization_gradient_norms):
-                    if layer is not None:
-                        if gradient_descent_method in ('AdaGrad', 'RMSProp'):
-                            kwargs = {k: lr * layer_gradient[k] / np.sqrt(past_layer_gradient_norm[k]+1e-6) for k in layer_gradient}
-                            layer.update(**kwargs)
-                        else:
-                            kwargs = {k: lr * layer_gradient[k] for k in layer_gradient}
-                            layer.update(**kwargs)
+                batch_normalization_gradients = batch_norm_optimizer.update({'gradients': batch_normalization_gradients}, learning_rate=learning_rate)
+                for batch_norm, batch_norm_gradient in zip(self.batch_normalization_layers, batch_normalization_gradients['gradients']):
+                    if batch_norm is not None:
+                        batch_norm.update(**batch_norm_gradient)
                      
             # Evaluation
             Y_pred = self.predict(X)
@@ -396,6 +444,7 @@ class RegressionRecurrentNeuralNetwork:
                 metrics['val_mse'] = mse(Y_val, Y_pred_val, axis=(0,1))
             self.history.append(metrics)
             
+            # Verbose
             if verbose and (e+1) % 10 == 0:
                 print(f"Epoch {e+1}/{epochs} - train loss = {metrics['train_mse'].mean():.4f}" + (f", validation loss = {metrics['val_mse'].mean():.4f}" if 'val_mse' in metrics else ""))
         
